37002308a7772ee752fe385044af64354f4e1148
Akhilesh Pal <palakhilesh466@gmail.com>
2024-01-28 19:29:56 +0530

    solution of important array problems

---
+++ 1Darray/MaxSumSubarray.java
@@ -0,0 +1,24 @@
+public class MaxSumSubarray {
+    public static void maxSum(int a[]){
+       int maxsum = Integer.MIN_VALUE;
+       int prefix[] = new int [a.length];
+       prefix[0] = a[0];
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = a[i]+prefix[i-1]; 
+        }
+        int sum = 0;
+        for (int i = 0; i < prefix.length; i++) {
+            int start = i;
+            for (int j = i; j < prefix.length; j++) {
+                int end = j;
+                sum = start==0?prefix[end]:prefix[end]-prefix[start-1];
+                maxsum = Math.max(sum,maxsum);
+            }
+        }
+        System.out.println("maxsum : "+maxsum);
+    }
+    public static void main(String[] args) {
+        int a[] = {1,-2,6,-1,3};
+        maxSum(a);
+    }
+}
