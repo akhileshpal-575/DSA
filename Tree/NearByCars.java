7d5e01344738bcf5bcda413c33944273c105c61f
Akhilesh Pal <palakhilesh466@gmail.com>
2024-06-01 15:35:04 +0530

    find nearby cars using priority Queue

---
+++ Tree/NearByCars.java
@@ -0,0 +1,35 @@
+import java.util.PriorityQueue;
+
+public class NearByCars {
+    
+    static class Point implements Comparable<Point>{
+        int x;
+        int y;
+        int distance;
+        int idx;
+         Point(int x, int y, int distance, int idx){
+          this.x = x;
+          this.y = y;
+          this.distance = distance;
+          this.idx = idx;    
+        }
+         public int compareTo(Point p2){
+            return this.distance - p2.distance;
+         }
+        }
+
+    public static void main(String[] args) {
+        int pts[][] = {{3,3},{5,-1},{-2,4}};
+        int k = 2;
+        PriorityQueue<Point>pq = new PriorityQueue<>();
+        for(int i = 0; i<pts.length; i++){
+            int distsq = pts[i][0]*pts[i][0] + pts[i][1]*pts[i][1];
+            pq.add(new Point(pts[i][0], pts[i][1],distsq,i));
+        }
+
+        for(int i= 0 ; i<k; i++){
+            System.out.println("C"+pq.remove().idx);
+        }
+
+    }
+}
